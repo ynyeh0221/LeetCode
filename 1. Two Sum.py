bb9e8f1d7964cdedc26323d9c78a31e124003cd8
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-13 10:02:33 -0400

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic={}
+        for i in xrange(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]]=i
+        for i in xrange(len(nums)):
+            if target-nums[i] in dic and i!=dic[target-nums[i]]:
+                return sorted([i, dic[target-nums[i]]])
