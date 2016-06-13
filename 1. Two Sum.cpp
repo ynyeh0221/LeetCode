11e72267e862d24c83c725a0750cf49f7d47d502
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-13 10:26:37 -0400

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> dic;
+        vector <int> res;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ( dic.count(nums[i]) == 0 )
+                dic[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ( dic.count( target-nums[i] ) > 0 && i != dic[target-nums[i]] )
+            {
+                res.push_back( i );
+                res.push_back( dic[target-nums[i]] );
+                sort( res.begin(), res.end() );
+                return res;
+            }   
+        }
+        return res;
+    }
+};
