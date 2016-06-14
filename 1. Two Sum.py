2be5533b5b14392011bf9f44399a698d850e86fa
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-14 12:10:39 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        dic={}
+        dic = {}
         for i in xrange(len(nums)):
             if nums[i] not in dic:
-                dic[nums[i]]=i
+                dic[nums[i]] = i
         for i in xrange(len(nums)):
-            if target-nums[i] in dic and i!=dic[target-nums[i]]:
-                return sorted([i, dic[target-nums[i]]])
+            if target - nums[i] in dic and i > dic[target-nums[i]]:
+                return [dic[target-nums[i]], i]
