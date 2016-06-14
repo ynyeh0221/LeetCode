875b46b94f4be8529311798db22413f429df3aa8
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-13 20:44:44 -0400

    Create 322. Coin Change.cpp

---
+++ 322. Coin Change.cpp
@@ -0,0 +1,21 @@
+#include <algorithm> 
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0)
+            return 0;
+        vector<int> T (amount + 1, amount + 1);
+        T[0] = 0;
+        for(int a = 1; a < amount + 1; a++)
+        {
+            for (int c = 0; c < coins.size(); c++)
+            {
+                if (coins[c] <= a)
+                    T[a] = min(T[a - coins[c]] + 1, T[a]);
+            }
+        }
+        if (T[amount] == amount + 1)
+            return -1;
+        return T[amount];
+    }
+};
