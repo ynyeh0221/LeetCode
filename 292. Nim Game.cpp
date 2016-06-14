2d76dd04f464087b1b487779ca90058b996ec022
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-13 22:25:56 -0400

    Update 292. Nim Game.cpp

--- 292. Nim Game.cpp
+++ 292. Nim Game.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        if (n % 4 == 0)
-            return false;
-        return true;
+        return n % 4 == 0 ? false : true;
     }
 };
