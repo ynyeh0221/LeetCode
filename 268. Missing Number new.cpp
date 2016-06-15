9bcac03502175be8ccca205ac682ca3210b71cda
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-15 13:23:11 -0400

    Create 268. Missing Number new.cpp

---
+++ 268. Missing Number new.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = n;
+        for (int i = 0; i < n; i++)
+        {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
