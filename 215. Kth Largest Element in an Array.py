bc677fcab2589b2906a2ddc4c9b3f84ed6361158
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-15 12:22:18 -0400

    Create 215. Kth Largest Element in an Array.py

---
+++ 215. Kth Largest Element in an Array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums = sorted(nums, reverse = True)
+        return nums[k-1]
