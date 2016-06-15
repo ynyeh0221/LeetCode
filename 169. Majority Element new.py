58d2ab8da9c0c258aee418f306a6bf78abc1074c
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-15 14:46:47 -0400

    Create 169. Majority Element new.py

---
+++ 169. Majority Element new.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 0
+            dic[i] += 1
+            if dic[i] > len(nums)/2:
+                return i
