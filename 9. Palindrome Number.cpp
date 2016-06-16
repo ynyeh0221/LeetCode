0febab1406abfc5e7fc5029e0a8632261b8432ba
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-16 10:39:53 -0400

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x >= 0 && x / 10 == 0)
+            return true;
+        if (x >= 0 && x % 10 == 0) // 10, 100, ...
+            return false;
+        int temp = 0;
+        while (x > 0)
+        {
+            temp = temp * 10 + x % 10;
+            if (x == temp || x / 10 == temp) //total digits is even or odd
+                return true;
+            x /= 10;
+        }
+        return false;
+    }
+};
