e03dd2efc4b9ac61f592cf4b1f6878ccf4841f62
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-15 20:42:00 -0400

    Update 279. Perfect Squares.py

--- 279. Perfect Squares.py
+++ 279. Perfect Squares.py
@@ -1,6 +1,5 @@
 # timeout
 
-import math
 class Solution(object):
     def numSquares(self, n):
         """
@@ -9,9 +8,9 @@ class Solution(object):
         """
         T = [n+1 for i in xrange(n+1)]
         for i in xrange(1, n + 1):
-            if int(math.sqrt(i))**2 == i:
-                T[i] = 1
-            else:
-                for j in xrange(1, i + 1):
-                    T[i] = min(T[i], T[i-j] + T[j])
+            if i*i <= n:
+                T[i*i] = 1
+        for i in xrange(1, n + 1):
+            for j in xrange(1, i):
+                T[i] = min(T[i], T[i-j] + T[j])
         return T[n]
