aa9af8c71adb731fe58a06086f3a9412d0abf0ce
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-18 10:16:20 -0400

    Update 198. House Robber new.py

--- 198. House Robber new.py
+++ 198. House Robber new.py
@@ -5,12 +5,8 @@ class Solution(object):
         :rtype: int
         """
         T = [0 for i in xrange(len(nums) + 1)]
-        if len(nums) == 0:
-            return 0
-        elif len(nums) == 1 or len(nums) == 2:
-            return max(nums)
-            
-        T[1] = nums[0]
+        if len(nums) > 0:
+            T[1] = nums[0]
         for i in xrange(2, len(nums) + 1):
             T[i] = max(T[i - 1], T[i - 2] + nums[i - 1])
         return T[len(nums)]
