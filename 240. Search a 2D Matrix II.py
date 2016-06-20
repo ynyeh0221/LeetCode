a780bfaf994b8fe1a5f30261b33b146774f61950
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-20 10:30:55 -0400

    Create 240. Search a 2D Matrix II.py

---
+++ 240. Search a 2D Matrix II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        i, j = 0, len(matrix[0])-1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+        return False
