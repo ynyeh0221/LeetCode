047f99ce6b6dc2e5d310989ec7400448a4844248
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-21 12:46:36 -0400

    Create 349. Intersection of Two Arrays.cpp

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set <int> s1 (nums1.begin(), nums1.end());
+        set <int> s2 (nums2.begin(), nums2.end());
+        vector <int> res;
+        set_intersection (s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(res));
+        return res;
+    }
+};
