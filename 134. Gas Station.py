1a11c943e9d22bfeb5afb918727d902f252b5f4b
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-22 18:12:26 -0400

    Create 134. Gas Station.py

---
+++ 134. Gas Station.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        tank = [gas[i] - cost[i] for i in xrange(len(gas))]
+        if sum(tank) < 0:
+            return -1
+        i = 0
+        while i < len(tank):
+            if tank[i] < 0: # only tank[i] >= 0 can be start point
+                i += 1
+            temp = 0
+            for j in xrange(i, len(tank)):
+                if temp < 0:
+                    break
+                temp += tank[j]
+            for j in xrange(i):
+                if temp < 0:
+                    break
+                temp += tank[j]
+            if temp >= 0:
+                return i
+            i += 1
+        return -1
