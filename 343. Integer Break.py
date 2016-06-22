055e121d935cf1e066c4dea26a2a3a5ee16ac6d1
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-22 15:14:47 -0400

    Create 343. Integer Break.py

---
+++ 343. Integer Break.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        T = [0 for i in xrange(n+1)]
+        if n == 2:
+            return 1
+        elif n == 3:
+            return 2
+        elif n == 4:
+            return 4
+        elif n == 5:
+            return 6
+        elif n == 6:
+            return 9
+        T[2] = 1
+        T[3] = 2
+        T[4] = 4
+        T[5] = 6
+        T[6] = 9
+        for i in xrange(7, n+1):
+            T[i] = T[i-3] * 3
+        return T[n]
