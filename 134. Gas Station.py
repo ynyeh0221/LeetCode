3382802c52ed0d3d45f67ddbae00ed1e0b92c9dc
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-22 18:14:05 -0400

    Update 134. Gas Station.py

--- 134. Gas Station.py
+++ 134. Gas Station.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        tank = [gas[i] - cost[i] for i in xrange(len(gas))]
+        tank = [gas[i] - cost[i] for i in xrange(len(gas))] # current gas in tank
         if sum(tank) < 0:
             return -1
         i = 0
