de24a4fc8319ff10571df130f6dbaf6c9d71038d
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-22 11:54:33 -0400

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,4 +1,3 @@
-import sys
 class Solution(object):
     def maxSubArray(self, nums):
         """
@@ -6,8 +5,8 @@ class Solution(object):
         :rtype: int
         """
         T = [0 for i in xrange(len(nums) + 1)]
-        T[0], res = -sys.maxint, -sys.maxint
-        for i in xrange(1, len(nums)+1):
+        T[1], res = nums[0], nums[0]
+        for i in xrange(2, len(nums)+1):
             T[i] = max(T[i-1]+nums[i-1], nums[i-1])
             res = max(res, T[i])
         return res
