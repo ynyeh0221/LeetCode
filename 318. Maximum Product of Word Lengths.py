ab0697e183dda0d643c309938bbfaddd38f5db15
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-23 15:42:04 -0400

    Update 318. Maximum Product of Word Lengths.py

--- 318. Maximum Product of Word Lengths.py
+++ 318. Maximum Product of Word Lengths.py
@@ -1,5 +1,3 @@
-# timeout
-
 class Solution(object):
     def maxProduct(self, words):
         """
@@ -8,19 +6,13 @@ class Solution(object):
         """
         wordlist = []
         for i in words:
-            letters = [0 for k in xrange(26)]
+            letters = 0
             for j in i:
-                letters[ord(j) - ord('a')] = 1
-            wordlist += [letters[:]]
+                letters|= 1 << ord(j) - ord('a')
+            wordlist += [letters]
         res = 0
         for i in xrange(len(wordlist)):
             for j in xrange(i+1, len(wordlist)):
-                different = 0
-                for k in xrange(len(wordlist[i])):
-                    if wordlist[i][k] & wordlist[j][k] == 1:
-                        break
-                    else:
-                        different += 1
-                if different == 26:
+                if wordlist[i] & wordlist[j] == 0:
                     res = max(res, len(words[i]) * len(words[j]))
         return res
