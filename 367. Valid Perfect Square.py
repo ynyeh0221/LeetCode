5bcde067467dd8048da591c37ceee1655e0e6894
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-26 14:13:19 -0400

    Create 367. Valid Perfect Square.py

---
+++ 367. Valid Perfect Square.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        l = 0
+        r = num/2 + 1
+        while l <= r:
+            mid = (l + r)/2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
