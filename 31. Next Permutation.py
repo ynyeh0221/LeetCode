1bffb95a05947400758d985a7abbe205b9dec1e4
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-06-29 20:52:52 -0400

    Create 31. Next Permutation.py

---
+++ 31. Next Permutation.py
@@ -0,0 +1,22 @@
+import sys
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        ind1 = len(nums)-1
+        for i in xrange(len(nums)-1, -1, -1):
+            if nums[i] > nums[i-1]:
+                ind1 = i - 1
+                break
+        ind2 = len(nums)-1
+        minn = sys.maxint
+        for j in xrange(ind1+1, len(nums)):
+            if nums[j] < minn and nums[j] > nums[ind1]:
+                minn = nums[j]
+                ind2 = j
+        nums[ind1], nums[ind2] = nums[ind2], nums[ind1]
+        temp = sorted(nums[ind1+1:])
+        for i in xrange(ind1+1, len(nums)):
+            nums[i] = temp[i-ind1-1]
