f1ba35384e47ead536e4e4f425fd538a8fc22081
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-07-09 22:21:35 -0400

    Create 278. First Bad Version.js

---
+++ 278. First Bad Version.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var l = 1; 
+        var r = n;
+        while (l < r)
+        {
+            var mid = l + Math.floor((r-l) / 2);
+            if (isBadVersion(mid))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    };
+};
